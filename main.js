b67974054b9fea1b83c38984cd8c089c2d57ca45
tabares7 <pedrotabares777@gmail.com>
2024-01-10 18:03:59 -0600

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ main.js
@@ -0,0 +1,128 @@
+const winner = [
+    // Horizontales
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    //Verticales
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    // Diagonales
+    [1, 5, 9],
+    [3, 5, 7]
+]
+const holes = document.querySelectorAll('.box');
+const resetBtn = document.querySelector('#restart');
+const scoreMarker = document.getElementById('marker'); //scoreM
+const score1 = document.getElementById('player1-puntuation');
+const score2 = document.getElementById('player2-puntuation');
+
+
+var turn = 0;
+var playerWinner = "";
+
+let player1 = [];
+let player2 = [];
+
+var score1Count = 0;
+var score2Count = 0;
+
+
+
+const turnToggle = ()=>{
+    if(turn == 0){
+        turn = 1;
+    }else{
+        turn = 0;
+    }
+}
+
+const playedCheck = (e)=>{
+    if(e.target.classList.contains('player1') || e.target.classList.contains('player2')){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+const reset = ()=>{
+    player1 = [];
+    player2 = [];
+    turn = 0;
+    scoreMarker.innerHTML = "";
+    scoreMarker.classList.remove('marker--visible');
+    playerWinner = "";
+    holes.forEach((hole)=>{
+        hole.classList.remove('player1');
+        hole.classList.remove('player2');
+    })
+}
+
+const checkDraw = ()=>{
+    if(player1.length + player2.length == 9){
+        scoreMarker.innerHTML = `It's a draw!!`;
+        scoreMarker.classList.add('marker--visible');
+    }
+}
+
+const checkWinner = (player)=>{
+    checkDraw();
+    if(player.length >= 3){ 
+        winner.forEach((win)=>{
+            let count = 0;
+            player.forEach((p)=>{
+                if(win.includes(p)){
+                    count++;
+                }
+            })
+            if(count == 3){
+                if  (turn == 0){
+                    playerWinner = "Red";
+                    scoreMarker.innerHTML += `${playerWinner} Wins!! `;
+                    scoreMarker.classList.add('marker--visible');
+                    score1Count++;
+                    score1.innerHTML = score1Count;
+                    
+                    return;
+                    
+                }else {
+                    playerWinner = "Green";
+                    scoreMarker.innerHTML += `${playerWinner} Wins!!`;
+                    scoreMarker.classList.add('marker--visible');
+                    score2Count++;
+                    score2.innerHTML = score2Count;
+                    
+                    return;
+                }   
+        }
+    })
+}
+checkDraw();
+}
+
+
+holes.forEach((hole)=>{
+    hole.addEventListener('click', (e)=>{
+        if(turn == 0){
+            if(playedCheck(e)){
+                return;
+            }
+            e.target.classList.add('player1');
+            player1.push(parseInt(e.target.id));
+            checkWinner(player1);
+            turnToggle();
+    }else{
+        if(playedCheck(e)){
+            return;
+        }
+        e.target.classList.add('player2');
+        player2.push(parseInt(e.target.id));
+        checkWinner(player2);
+        turnToggle();
+    }
+})})
+
+
+
+resetBtn.addEventListener('click', reset);
